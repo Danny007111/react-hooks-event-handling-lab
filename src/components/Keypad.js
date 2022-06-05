fc06415985883e69205237b992c9752bde0daeaf
Danny Rodriguez <danny007111@hotmail.com>
2022-06-04 22:51:02 -0500

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function enterPassword(){
+        console.log('Entering password...');
+    };
+    return (
+        <>
+        <input type="password" onChange={enterPassword}></input>
+        </>
+    )
+};
+
+export default Keypad;
